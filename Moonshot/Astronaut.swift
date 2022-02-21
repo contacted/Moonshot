22048a730de4851be7d131c57dc4a80bd02b2cad
Eugene <contact.duk@gmail.com>
2022-02-21 13:42:10 +0300

    Adding initial source code

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Eugene on 4.01.22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
